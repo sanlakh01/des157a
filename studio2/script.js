fa69f59fb4f05addf7262d5479d396500b7eb954
Santhoshi <san.lakh1306@gmail.com>
2023-02-25 23:08:09 -0800

    mostly working!

--- studio2/script.js
+++ studio2/script.js
@@ -3,30 +3,68 @@
     "use strict";
     console.log("reading js");
 
-    const container = document.querySelector("#container");
-    const hotSpots = document.querySelectorAll("#container div");
-    const theImg = document.querySelector("div img");
+    const overlay = document.querySelectorAll("#overlay");
 
-    hotSpots.forEach(function (eachSpot){
-        eachSpot.addEventListener("mouseover", zoomPhoto);
-        eachSpot.addEventListener("mouseout", function(){
-            theImg.className = "start";
-        });
+    //the following are for the divs appearing.
+    document.querySelector("#green").addEventListener("mouseenter", function(event){
+        event.preventDefault();
+        overlay[0].className = "show";
+        console.log(overlay[0]);
+        //document.getElementById("oo").className = "show";
     });
 
-    function zoomPhoto(event){
-        const earring = event.target.id;
-        console.log(earring);
-        if (earring){
-            theImg.className = "green";
-        }
+    document.querySelector("#copper").addEventListener("mouseenter", function(event){
+        event.preventDefault();
+        overlay[1].className = "show";
+        console.log(overlay[1]);
+    });
 
-        // switch(earring){
-        //     case "green": theImg.className = "green"; break;
-        //     case "topright": theImg.className = "topright"; break; 
-        //     case "bottomleft": theImg.className = "bottomleft"; break; 
-        //     case "bottomright": theImg.className = "bottomright"; break; 
-        //     case "center": theImg.className = "center"; break; 
-        // }
-    }
+    document.querySelector("#cuffs").addEventListener("mouseenter", function(event){
+        event.preventDefault();
+        overlay[2].className = "show";
+        console.log(overlay[2]);
+    });
+
+    document.querySelector("#phases").addEventListener("mouseenter", function(event){
+        event.preventDefault();
+        overlay[3].className = "show";
+        console.log(overlay[3]);
+    });
+
+    document.querySelector("#black").addEventListener("mouseenter", function(event){
+        event.preventDefault();
+        overlay[4].className = "show";
+        console.log(overlay[4]);
+    });
+
+    //the following are for the divs disappearing.
+    document.querySelector("#green").addEventListener("mouseout", function(event){
+        event.preventDefault();
+        overlay[0].className = "hide";
+        console.log(overlay[0]);
+    });
+
+    document.querySelector("#copper").addEventListener("mouseout", function(event){
+        event.preventDefault();
+        overlay[1].className = "hide";
+        console.log(overlay[1]);
+    });
+
+    document.querySelector("#cuffs").addEventListener("mouseout", function(event){
+        event.preventDefault();
+        overlay[2].className = "hide";
+        console.log(overlay[2]);
+    });
+
+    document.querySelector("#phases").addEventListener("mouseout", function(event){
+        event.preventDefault();
+        overlay[3].className = "hide";
+        console.log(overlay[3]);
+    });
+
+    document.querySelector("#black").addEventListener("mouseout", function(event){
+        event.preventDefault();
+        overlay[4].className = "hide";
+        console.log(overlay[4]);
+    });
 })();
