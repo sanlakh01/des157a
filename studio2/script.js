e160ca9fa891580f8ef7d4272dff8b35656defb7
Santhoshi <san.lakh1306@gmail.com>
2023-02-25 20:33:03 -0800

    so many mods pls leeave me alone

--- studio2/script.js
+++ studio2/script.js
@@ -2,4 +2,31 @@
 
     "use strict";
     console.log("reading js");
+
+    const container = document.querySelector("#container");
+    const hotSpots = document.querySelectorAll("#container div");
+    const theImg = document.querySelector("div img");
+
+    hotSpots.forEach(function (eachSpot){
+        eachSpot.addEventListener("mouseover", zoomPhoto);
+        eachSpot.addEventListener("mouseout", function(){
+            theImg.className = "start";
+        });
+    });
+
+    function zoomPhoto(event){
+        const earring = event.target.id;
+        console.log(earring);
+        if (earring){
+            theImg.className = "green";
+        }
+
+        // switch(earring){
+        //     case "green": theImg.className = "green"; break;
+        //     case "topright": theImg.className = "topright"; break; 
+        //     case "bottomleft": theImg.className = "bottomleft"; break; 
+        //     case "bottomright": theImg.className = "bottomright"; break; 
+        //     case "center": theImg.className = "center"; break; 
+        // }
+    }
 })();
