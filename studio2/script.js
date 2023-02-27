71d76fb5e843b6154548ff1f20e63ef105c6230e
Santhoshi <san.lakh1306@gmail.com>
2023-02-27 12:41:35 -0800

    df;j

--- studio2/script.js
+++ studio2/script.js
@@ -3,8 +3,6 @@
     "use strict";
     console.log("reading js");
 
-    const overlay = document.querySelectorAll("#overlay");
-
     //THE FOLLOWING FUNCTION LOADS THE PRE-LOAD ANIMATION
     window.addEventListener("load", function(){
         const preloader = document.getElementById('preloader');
@@ -23,8 +21,8 @@
         event.preventDefault();
 
         //OVERLAY DIALOG BOX
-        overlay[0].className = "show";
-        console.log(overlay[0]);
+        document.querySelector("#overlay1").className = "show";
+        console.log(`overlay 1 - green moon phases earrings`);
         
         //ZOOM IN TO GREEN EARRING
         document.querySelector("#image").className = "one";
@@ -33,9 +31,9 @@
     document.querySelector("#copper").addEventListener("mouseover", function(event){
         event.preventDefault();
 
-
-        overlay[1].className = "show";
-        console.log(overlay[1]);
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay2").className = "show";
+        console.log(`overlay 2 - copper moon earrings`);
 
         //ZOOM IN TO COPPER EARRING
         document.querySelector("#image").className = "two";
@@ -43,8 +41,10 @@
 
     document.querySelector("#cuffs").addEventListener("mouseover", function(event){
         event.preventDefault();
-        overlay[2].className = "show";
-        console.log(overlay[2]);
+        
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay3").className = "show";
+        console.log(`overlay 3 - ear cuffs with dangles`);
 
         //ZOOM IN TO CUFFS
         document.querySelector("#image").className = "three";
@@ -52,8 +52,10 @@
 
     document.querySelector("#phases").addEventListener("mouseover", function(event){
         event.preventDefault();
-        overlay[3].className = "show";
-        console.log(overlay[3]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay4").className = "show";
+        console.log(`overlay 4 - white moon phase earrings`);
 
         //ZOOM IN TO WHITE EARRING
         document.querySelector("#image").className = "four";
@@ -61,8 +63,10 @@
 
     document.querySelector("#black").addEventListener("mouseover", function(event){
         event.preventDefault();
-        overlay[4].className = "show";
-        console.log(overlay[4]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay5").className = "show";
+        console.log(`overlay 5 - black moon earrings`);
 
         //ZOOM IN TO BLACK EARRING
         document.querySelector("#image").className = "five";
@@ -71,36 +75,56 @@
     //THE FOLLOWING 5 FUNCTIONS HAVE MOUSEOUT INTERACTIONS FOR ALL THE EARRINGS. 
     document.querySelector("#green").addEventListener("mouseout", function(event){
         event.preventDefault();
-        overlay[0].className = "hide";
-        console.log(overlay[0]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay1").className = "hide";
+        console.log(`overlay 1 CLOSE`);
+
+        //ZOOM OUT TO FULL IMAGE
         document.querySelector("#image").className = "start";
     });
 
     document.querySelector("#copper").addEventListener("mouseout", function(event){
         event.preventDefault();
-        overlay[1].className = "hide";
-        console.log(overlay[1]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay2").className = "hide";
+        console.log(`overlay 2 CLOSE`);
+
+        //ZOOM OUT TO FULL IMAGE
         document.querySelector("#image").className = "start";
     });
 
     document.querySelector("#cuffs").addEventListener("mouseout", function(event){
         event.preventDefault();
-        overlay[2].className = "hide";
-        console.log(overlay[2]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay3").className = "hide";
+        console.log(`overlay 3 CLOSE`);
+
+        //ZOOM OUT TO FULL IMAGE
         document.querySelector("#image").className = "start";
     });
 
     document.querySelector("#phases").addEventListener("mouseout", function(event){
         event.preventDefault();
-        overlay[3].className = "hide";
-        console.log(overlay[3]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay4").className = "hide";
+        console.log(`overlay 4 CLOSE`);
+
+        //ZOOM OUT TO FULL IMAGE
         document.querySelector("#image").className = "start";
     });
 
     document.querySelector("#black").addEventListener("mouseout", function(event){
         event.preventDefault();
-        overlay[4].className = "hide";
-        console.log(overlay[4]);
+
+        //OVERLAY DIALOG BOX
+        document.querySelector("#overlay5").className = "hide";
+        console.log(`overlay 5 CLOSE`);
+
+        //ZOOM OUT TO FULL IMAGE
         document.querySelector("#image").className = "start";
     });
 })();
