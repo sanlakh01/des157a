ae927da92f67088cc8b2bb49a6ed296a9eef2973
Santhoshi <san.lakh1306@gmail.com>
2023-03-06 21:17:09 -0800

    changes galore

--- studio3/script.js
+++ studio3/script.js
@@ -14,33 +14,11 @@
         score: [0,0],
         var1: 0,
         var2: 0,
-        sum: 0, 
+        sum: [0,0], 
         count: [0,0], //when it hits 8, player has no more attempts
         index: 0,
         fails: [0,0] //tallies the player with the least amount of fails
     }
-
-    //the following function closes the instruction overlay and shows the game area - assigns player
-    play.addEventListener("click", function(event){
-        
-        event.preventDefault();
-
-        overlay.className = "hide";
-
-        gameData.index = Math.round(Math.random()); //random generates b/w 0-1, round rounds up/down to 0 or 1
-        console.log(gameData.index);
-        //tells you who is playing
-        if(gameData.index){
-            //mona
-            document.querySelector("#two h3").className = "show";
-        }
-        else{
-            //cloud
-            document.querySelector("#one h3").className = "show";
-        }
-
-        setUpTurn();
-    })
 
     //the following function is for the information button to work
     info.addEventListener("click", function(event){
@@ -61,54 +39,98 @@
         }
     })
 
+    //the following function closes the instruction overlay and shows the game area - assigns player
+    play.addEventListener("click", function(event){
+        
+        event.preventDefault();
+
+        overlay.className = "hide";
+
+        gameData.index = Math.round(Math.random()); //random generates b/w 0-1, round rounds up/down to 0 or 1
+        console.log(gameData.index);  //tells you who is playing
+
+        setUpTurn();
+    })
+
     //sets up the game area for the respective cat
     function setUpTurn(){
+
+        console.log("set up!");
 
         if(gameData.index){
             //mona
             mona.className = "show";
             mona.innerHTML = '<h3>your chance of waking <span style="color: #9A8479">mona</span> up is</h3>';
-            mona.innerHTML += "<h3>--</h3>";
-            mona.innerHTML += '<button id="chore">chore time!</button>';
+            mona.innerHTML += "<h3></h3>";
+            // mona.innerHTML += '<button id="chore">chore time!</button>';
             cloud.className = "hide";
         }
         else{
             //cloud
             cloud.className = "show";
-            cloud.innerHTML = '<h3>your chance of waking <span style="color: #707070">cloud</span> up is</h3>';
-            cloud.innerHTML += "<h3>--</h3>";
-            cloud.innerHTML += '<button id="chore">chore time!</button>';
+            // cloud.innerHTML = '<h3>your chance of waking <span style="color: #707070">cloud</span> up is</h3>';
+            cloud.innerHTML += "<h3></h3>";
+            // cloud.innerHTML += '<button id="chore">chore time!</button>';
             mona.className = "hide";
         }
 
         playGame();
     }
 
+    document.addEventListener("click", function(event){
+
+        event.stopPropagation();
+
+        if(event.target.id === "chore"){
+            setUpTurn();
+            console.log("chore");
+        }
+
+        if(event.target.id === "pass"){
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+
+                setUpTurn();
+                console.log("pass");
+        }
+    });
+
     //records random number generator, shows the total. 
     function playGame(){
 
+        if(gameData.count[0] === 8 || gameData.count[1] === 8){
+            checkWinningCondition();
+
+            return;
+        }
+
         gameData.count[gameData.index]++;
 
-        gameData.index ? (mona.innerHTML = '<h3>your chance of waking <span style="color: #9A8479">mona</span> up is</h3>') : (cloud.innerHTML = '<h3>your chance of waking <span style="color: #707070">cloud</span> up is</h3>');
+        if(gameData.index){
+            mona.innerHTML = '<h3>your chance of waking <span style="color: #9A8479">mona</span> up is</h3>';
+        }
+        else{
+            cloud.innerHTML = '<h3>your chance of waking <span style="color: #707070">cloud</span> up is</h3>';
+        }
 
         gameData.var1 = Math.floor(Math.random() * 50) + 1;
 
         gameData.var2 = Math.floor(Math.random() * 50) + 1;
 
-        gameData.sum = gameData.var1 + gameData.var2;
+        gameData.sum[gameData.index] = gameData.var1 + gameData.var2;
 
-        if(gameData.sum == 25 || gameData.sum == 50 || gameData.sum == 75 || gameData.sum == 100){
+        if(!(gameData.sum[gameData.index] % 2) && !(gameData.sum[gameData.index] % 3)){
+
             //failing condition
             if(gameData.index){
-                mona.innerHTML += '<h3 id="awake">Mona is awake!</h3>';
+                mona.innerHTML = '<h3>your chance of waking <span style="color: #9A8479">mona</span> up is</h3><h3 id="awake">Mona is awake!</h3>';
                 document.getElementById("awake").style.color = "#811919";
-                mona.innerHTML += `<h3>${gameData.score[gameData.index]}</h3>`;
+                mona.innerHTML += `<h3>${gameData.sum[gameData.index]}</h3>`;
                 mona.innerHTML += '<h3><span style="color: #707070">cloud&lsquo;s</span> turn</h3>';
             }
             else{
                 cloud.innerHTML += '<h3 id="awake">Cloud is awake!</h3>';
                 document.getElementById("awake").style.color = "#811919";
-                cloud.innerHTML += `<h3>${gameData.score[gameData.index]}</h3>`;
+                cloud.innerHTML += `<h3>${gameData.sum[gameData.index]}</h3>`;
                 cloud.innerHTML += '<h3><span style="color: #9A8479">mona&lsquo;s</span> turn</h3>';
             }
 
@@ -119,67 +141,60 @@
 
             setTimeout(setUpTurn, 3000);
         }
-        else if(gameData.sum == 11 || gameData.sum == 22 || gameData.sum == 33 || gameData.sum == 44 || gameData.sum == 55 || gameData.sum == 66 || gameData.sum == 77 || gameData.sum == 88 || gameData.sum == 99){
-            //semi fail condition
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+        else if(!(gameData.sum[gameData.index] % 3)){
 
+            //semi fail condition
+            
             if(gameData.index){
-                mona.innerHTML += `${gameData.score[gameData.index]}`;
+                mona.innerHTML += `<h3>${gameData.sum[gameData.index]}</h3>`;
                 mona.innerHTML += '<h3 id="semi">Mona was disturbed!</h3>';
                 mona.innerHTML += '<h3><span style="color: #707070">cloud&lsquo;s</span> turn</h3>';
             }
             else{
-                cloud.innerHTML += `${gameData.score[gameData.index]}`;
+                cloud.innerHTML += `<h3>${gameData.sum[gameData.index]}</h3>`;
                 cloud.innerHTML += '<h3 id="semi">Cloud was disturbed!</h3>';
                 cloud.innerHTML += '<h3><span style="color: #9A8479">mona&lsquo;s</span> turn</h3>';
                 
             }
-            
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
             gameData.fails[gameData.index]++;
             setTimeout(setUpTurn, 2000);
         }
         else{
-            gameData.score[gameData.index] +=gameData.sum;
+
+            gameData.count[gameData.index]++;
 
             if(gameData.index){
-                mona.innerHTML += `${gameData.score[gameData.index]}`;
+                mona.innerHTML += `<h3>${gameData.sum[gameData.index]}</h3>`;
                 mona.innerHTML += '<h3 id="sleep">Mona is asleep!</h3>';
                 document.getElementById("sleep").style.color = "#1D6716";
-                mona.innerHTML += '<button id="chore">chore time!</button> <button id="pass"><span style="color: #707070">cloud&lsquo;s</span> turn</button>';
+                mona.innerHTML += '<button id="chore">chore time!</button> <button id="pass">cloud&lsquo;s turn</button>';
             }
             else{
-                mona.innerHTML += `${gameData.score[gameData.index]}`;
+                cloud.innerHTML += `<h3>${gameData.sum[gameData.index]}</h3>`;
                 cloud.innerHTML += '<h3 id="sleep">Cloud is asleep!</h3>';
                 document.getElementById("sleep").style.color = "#1D6716";
-                cloud.innerHTML += '<button id="chore">chore time!</button> <button id="pass"><span style="color: #9A8479">mona&lsquo;s</span> turn</button>';
+                cloud.innerHTML += '<button id="chore">chore time!</button> <button id="pass">mona&lsquo;s turn</button>';                
             }
 
-            document.getElementById("chore").addEventListener("click", function(){
-                setUpTurn();
-            })
-
-            document.getElementById("pass").addEventListener("click", function(){
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            })
         }
+
+        gameData.sum[gameData.index] = 0;
 
     }
 
     function checkWinningCondition(){
 
-        if(gameData.count[index] === 8){
             //stop the game..
-            overlay.className = "show";
+        overlay.className = "show";
 
-            gameData.fails[0] > gameData.fails[1] ? overlay.innerHTML = "<h3>Cloud's parent was most successful. Enjoy his snores</h3>" : overlay.innerHTML = "<h3>Mona's parent was most successful. Enjoy her snores!</h3>"
+        gameData.fails[0] > gameData.fails[1] ? (overlay.innerHTML = "<h2>Cloud's parent was most successful. Enjoy his snores!</h2>") : (overlay.innerHTML = "<h2>Mona's parent was most successful. Enjoy her snores!</h2>");
 
-            overlay.innerHTML += '<button id="new">play again?</button>';
+        overlay.innerHTML += '<button id="new">play again?</button>';
 
-            document.getElementById("new").addEventListener("click", function(){
-                location.reload();
-            })
-        }
-
+        document.getElementById("new").addEventListener("click", function(){
+            location.reload();
+        })
     }
+
 })();
